091404edf17dbe020b0efc618a0421adf61e4e67
Andres Peña <ing.andres.felipe@hotmail.com>
2024-12-28 10:23:08 -0500

    Fix add Alert in ios and android

--- src/presentation/screens/alerts/AlertScreen.tsx
+++ src/presentation/screens/alerts/AlertScreen.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Alert, StyleSheet, View} from 'react-native';
 
-import prompt from 'react-native-prompt-android';
-
 import {CustomView} from '../../components/ui/CustomView';
 import {globalStyles} from '../../../config/theme/theme';
 import {Title} from '../../components/ui/Title';
 import {Button} from '../../components/ui/Button';
+import {showPrompt} from '../../../config/adapters/prompt.adapters';
 
 export const AlertScreen = () => {
   const createTwoButtonAlert = () =>
@@ -34,6 +33,22 @@ export const AlertScreen = () => {
     ]);
 
   const onShowPrompt = () => {
+    showPrompt({
+      title: 'Alert Title',
+      subtitle: 'My Alert Msg',
+      buttons: [
+        {text: 'ok', onPress: () => console.log('ok'), style: 'default'},
+        {
+          text: 'cancel',
+          onPress: () => console.log('cancel'),
+          style: 'default',
+        },
+      ],
+      prompType: 'plain-text',
+      placeholder: 'Input value',
+      defaultValue: 'default value',
+    });
+
     // Alert.prompt(
     //   'Alert Title',
     //   'My Alert Msg',
